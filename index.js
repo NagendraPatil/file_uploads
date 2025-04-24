478bbb7e865b465da39fa9a7906451c2da0917c0
Nagendra Patil <nagendrapatil@Mac.lan>
2025-04-24 09:40:03 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = 8000;
+const ejs = require("ejs");
+
+app.use(express.json());
+app.set("view engine", "ejs");
+app.set("views",path.resolve("./views"));
+
+app.get("/",(req,res)=>{
+    return res.render("homepage")
+})
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
