2aef74c9452d6f78e8a1c709ea2329ce4947397a
Nagendra Patil <nagendrapatil@Nagendras-MacBook-Air.local>
2025-04-30 20:35:37 +0530

    added homepage and file posting

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 const express = require("express");
 const path = require("path");
+const multer = require("multer");
+
 const app = express();
 const PORT = 8000;
-const ejs = require("ejs");
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    return cb(null, "./uploads/test");
+  },
+  filename: function (req, file, cb) {
+    return cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+const uploads = multer({ storage: storage });
 
 app.use(express.json());
-app.set("view engine", "ejs");
-app.set("views",path.resolve("./views"));
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/",(req,res)=>{
-    return res.render("homepage")
-})
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+app.get("/", (req, res) => {
+  return res.render("homepage");
+});
+
+app.post("/upload", uploads.single("pfp-file"), async (req, res) => {
+//   console.log(req.body, req.file);
+  return res.redirect("/");
+});
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
